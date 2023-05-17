f522baa85cbf1e5d1b25579f2a7c595c19d6f870
TOWHIDul Islam <towhidulislam2.bd@gmail.com>
2023-05-18 01:40:30 +0600

    gellary done

---
+++ src/pages/home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import useTitle from '../../../hooks/useTitle';
+
+const Home = () => {
+    useTitle("Home")
+    return (
+        <div>
+            <h1>THis is Home</h1>
+        </div>
+    );
+};
+
+export default Home;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home/Home/Home";
+import Layout from "../Layouts/Layout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Layout></Layout>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            },
+        ]
     },
 ]);
 export default router
