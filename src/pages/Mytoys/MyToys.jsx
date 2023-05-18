4859622799448a67e209cd531f43467a40ea42fc
TOWHIDul Islam <towhidulislam2.bd@gmail.com>
2023-05-19 00:01:40 +0600

    table responsive

--- src/pages/Mytoys/MyToys.jsx
+++ src/pages/Mytoys/MyToys.jsx
@@ -33,7 +33,7 @@ const MyToys = () => {
                     .then(res => res.json())
                     .then(data => {
                         if (data.deletedCount > 0) {
-                            const remainingdata = mytoys&&mytoys.filter(toy=>toy._id!==id)
+                            const remainingdata = mytoys && mytoys.filter(toy => toy._id !== id)
                             setMytoys(remainingdata)
                             Swal.fire(
                                 'Deleted!',
@@ -54,64 +54,66 @@ const MyToys = () => {
     }
     return (
         <div>
-            <table className="table w-full my-10">
-                {/* head */}
-                <thead>
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox " />
-                            </label>
-
-                        </th>
-                        <th>Toy Name and Info <br />Sub-category</th>
-                        <th>Seller Information</th>
-                        <th>Price</th>
-                        <th>Available Quantity</th>
-                        <th>Acton</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* row 1 */}
-                    {
-                        mytoys && mytoys.map(data => <tr key={data._id}>
+            <div className="overflow-x-auto w-full my-10">
+                <table className="table w-full my-10 table-normal">
+                    {/* head */}
+                    <thead>
+                        <tr>
                             <th>
                                 <label>
-                                    <input type="checkbox" className="checkbox" />
-                                    <button onClick={() => handelDelete(data._id)} className="btn ml-6 btn-circle btn-outline btn-warning">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                    </button>
+                                    <input type="checkbox" className="checkbox " />
                                 </label>
+
                             </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src={data.pictureURL} alt="Avatar Tailwind CSS Component" />
+                            <th>Toy Name and Info <br />Sub-category</th>
+                            <th>Seller Information</th>
+                            <th>Price</th>
+                            <th>Available Quantity</th>
+                            <th>Acton</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            mytoys && mytoys.map(data => <tr key={data._id}>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                        <button onClick={() => handelDelete(data._id)} className="btn ml-6 btn-circle btn-outline btn-warning">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                        </button>
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={data.pictureURL} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{data.name}</div>
+                                            <div className="text-sm opacity-50">{data.subcategory}</div>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">{data.name}</div>
-                                        <div className="text-sm opacity-50">{data.subcategory}</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <h2 className='text-xl'>{data.sellerName}</h2>
-                                <div>{data.sellerEmail}</div>
-                            </td>
-                            <td>{data.price}</td>
-                            <td>{data.availableQuantity}</td>
-                            <th>
-                                <Link to={`/updatetoys/${data._id}`}> <button className="btn btn-warning">Update</button></Link>
-                            </th>
-                        </tr>)
-                    }
+                                </td>
+                                <td>
+                                    <h2 className='text-xl'>{data.sellerName}</h2>
+                                    <div>{data.sellerEmail}</div>
+                                </td>
+                                <td>{data.price}</td>
+                                <td>{data.availableQuantity}</td>
+                                <th>
+                                    <Link to={`/updatetoys/${data._id}`}> <button className="btn btn-warning">Update</button></Link>
+                                </th>
+                            </tr>)
+                        }
 
-                </tbody>
+                    </tbody>
 
 
-            </table>
+                </table>
+            </div>
         </div>
     );
 };
